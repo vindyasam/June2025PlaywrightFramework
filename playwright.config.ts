50e88314daeb8ea26a8a24bce4a7dfad69fc3310
vinyasam <samaranayake.vindya@gmail.com>
2025-12-02 18:27:22 +0530

    Fix CI: run chrome headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   use: {
     
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     screenshot: 'on',
     video: 'on',
     baseURL: 'https://naveenautomationlabs.com/opencart/index.php',
